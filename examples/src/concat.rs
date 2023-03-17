c71e50f9bcd9c5b8dd9e9434de5167243aa312cd
csh <458761603@qq.com>
2023-03-17 15:11:27 +0800

    fix: example

--- examples/src/concat.rs
+++ examples/src/concat.rs
@@ -1,7 +1,6 @@
-use libsql_bindgen::libsql_bindgen;
-use libsql_wasm_abi::*;
+use libsql_bindgen::*;
 
-#[libsql_bindgen]
+#[libsql_bindgen::libsql_bindgen]
 pub fn concat(s1: String, s2: String) -> String {
     let mut ret = s1.clone();
     ret += &s2;
